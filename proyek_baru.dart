a399cd6c28438f4ac37283756b02a82efa763525
YosionBestyMarpaung <147596895+YosionBestyMarpaung@users.noreply.github.com>
2025-02-28 16:54:03 +0700

    Create proyek_baru.dart

---
+++ proyek_baru.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I Am Hunggry"),
+          backgroundColor: const Color.fromARGB(255, 210, 204, 134),
+        ),
